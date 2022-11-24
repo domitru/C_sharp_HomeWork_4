1ac89e416447a5037d7ab139444ed9885aabd9e3
Dmitry Basov <dom.it.ru@yandex.ru>
2022-11-25 00:36:29 +0300

    added

--- Task025/Program.cs
+++ Task025/Program.cs
@@ -3,3 +3,17 @@
 // пример:
 //             3, 5 -> 243 (3⁵)
 //              2, 4 -> 16
+
+
+Console.WriteLine("Ведите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите число B: ");
+int stepenB = Convert.ToInt32(Console.ReadLine());
+int i;
+int As = A*A;
+for (i = 2; i < stepenB; i++)
+   As = As * A;
+Console.WriteLine(As);
+
+
+
