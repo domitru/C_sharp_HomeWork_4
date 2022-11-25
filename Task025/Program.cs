8074d7213c031ab7d80a7dd7994aa6d14cc38791
Dmitry Basov <dom.it.ru@yandex.ru>
2022-11-25 13:23:38 +0300

    added

--- Task025/Program.cs
+++ Task025/Program.cs
@@ -4,16 +4,20 @@
 //             3, 5 -> 243 (3⁵)
 //              2, 4 -> 16
 
-
-Console.WriteLine("Ведите число А: ");
-int A = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Ведите число B: ");
-int stepenB = Convert.ToInt32(Console.ReadLine());
-int i;
-int As = A*A;
-for (i = 2; i < stepenB; i++)
+int NumPow (int A, int stepenB)         //  Метод ,число в степени.
+{
+   int i;
+   int As = A*A;
+   for (i = 2; i < stepenB; i++)
    As = As * A;
-Console.WriteLine(As);
+   return As;
+}
+Console.Write("Ведите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите число B: ");
+int stepenB = Convert.ToInt32(Console.ReadLine());
+
+int result = NumPow(A, stepenB);        // Вызов метода число в степени.
 
 
-
+Console.WriteLine(A + " в степени " + stepenB +" = " + result);
